5090f03896431a07d31cf68096d81c9e6d8a7770
Matthew Smith <mtscout6@gmail.com>
2015-03-31 09:51:09 -0600

    Changed tests to be async

--- test/parser.js
+++ test/parser.js
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
 import fs from 'fs';
+import fsp from 'fs-promise';
 import path from 'path';
 import parser from '../src/parser';
 import _ from 'lodash';
@@ -7,29 +8,32 @@ import _ from 'lodash';
 const inputFixturesDir = path.join(__dirname, 'fixtures', 'input');
 const outputFixturesDir = path.join(__dirname, 'fixtures', 'output', 'parser');
 
-function createTest(fixtureName) {
-  return function(result) {
-    return result.data.map((parserData, index) => {
-      let outputFixtureFilename = index + '.css';
-      let outputFixtureFilepath = path.join(outputFixturesDir, fixtureName, outputFixtureFilename);
-      let outputFixtureData = fs.readFileSync(outputFixtureFilepath, {
-        encoding: 'utf8'
+function testParserResults(fixtureName, result) {
+  return result.data.map((parserData, index) => {
+    let outputFixtureFilename = index + '.css';
+    let outputFixtureFilepath = path.join(outputFixturesDir, fixtureName, outputFixtureFilename);
+
+    return fsp.readFile(outputFixtureFilepath, { encoding: 'utf8' })
+      .then(outputFixtureData => {
+        parserData = parserData.replace(/\s+/g, '');
+        outputFixtureData = outputFixtureData.replace(/\s+/g, '');
+        expect(parserData).to.equal(outputFixtureData);
       });
-      parserData = parserData.replace(/\s+/g, '');
-      outputFixtureData = outputFixtureData.replace(/\s+/g, '');
-      return expect(parserData).to.equal(outputFixtureData);
-    });
-  };
+  });
 }
 
 function buildContext(fixtureName, filename) {
   return {
     [`with data from the fixture "${filename}"`]: {
-      'should parse the CSS correctly'() {
+      'should parse the CSS correctly'(done) {
         let inputFixtureFilepath = path.join(inputFixturesDir, filename);
-        let inputFixtureData = fs.readFileSync(inputFixtureFilepath, { encoding: 'utf8' });
-        let result = parser(inputFixtureData);
-        return createTest(result);
+        fsp.readFile(inputFixtureFilepath, { encoding: 'utf8' })
+          .then(inputFixtureData => {
+            let result = parser(inputFixtureData);
+            return testParserResults(fixtureName, result);
+          })
+          .then(() => done())
+          .catch(err => done(err));
       }
     }
   };
