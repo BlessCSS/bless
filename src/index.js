58d25054638125d54ac1f7df487c65de300cb622
Matthew Smith <mtscout6@gmail.com>
2015-04-21 09:24:27 -0600

    Use babel-runtime instead of polyfill.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
-// CLI Runner will work with pre-transpiled babel output, thus needing this
-// polyfill. Not necessary when running unit tests. Babel will throw if this
-// module is loaded twice. Using ES6 dynamic module loading will not work
-// either since that polyfill will not be available till after a polyfill is
-// loaded. Hence the reason for falling back to CommonJS style module import.
-if (!global._babelPolyfill) {
-  require('babel/polyfill');
-}
-
 import fsp from 'fs-promise';
 import chunker from './chunk';
 import _ from 'lodash';
