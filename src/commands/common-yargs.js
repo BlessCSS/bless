e50c447484e0190c6adde372f7ae7efb0f11cd48
Matthew Smith <mtscout6@gmail.com>
2015-09-25 13:55:10 -0600

    Fix Missed CSS Band-aid to BlessCSS

--- src/commands/common-yargs.js
+++ src/commands/common-yargs.js
@@ -2,7 +2,7 @@ import yargs from 'yargs';
 import { version } from '../../package.json';
 
 export default function appendCommon() {
-  yargs.usage(`CSS Band Aid v${version}`.magenta + ` - Tools to ensure CSS files meet IE 6-9 selector limit restrictions.`)
+  yargs.usage(`BlessCSS v${version}`.magenta + ` - Tools to ensure CSS files meet IE 6-9 selector limit restrictions.`)
     .epilogue('For additional information see http://blesscss.com')
     .wrap(null);
 }
